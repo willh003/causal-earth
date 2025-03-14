69c3e79e2cedd8420623bcd7cdb8d0abc262a568
Will Pinstrup Huey <willhu003@gmail.com>
2025-03-14 15:24:39 -0400

    change download to v2

--- causal-earth/scripts/download/download.py
+++ causal-earth/scripts/download/download.py
@@ -1,124 +1,144 @@
 
-from typing import Sequence, Union, Dict, List
-import hashlib
-import pickle
-import os
-import urllib.request
+import numpy as np
+import s3fs
+import xarray as xr
+from pathlib import Path
 from tqdm import tqdm
-import tarfile
-from links import DOWNLOAD_LINKS
 
+SPLITS = {
+    "earthnet2021x": ["train","iid","ood","extreme","seasonal"]
+}
 
-class DownloadProgressBar(tqdm):
-    def update_to(self, b=1, bsize=1, tsize=None):
-        if tsize is not None:
-            self.total = tsize
-        self.update(b * bsize - self.n)
-
-def get_sha_of_file(file: str, buf_size: int = 100*1024*1024):
-    sha = hashlib.sha256()
-    with open(file, 'rb') as f:
-        while True:
-            data = f.read(buf_size)
-            if not data:
-                break
-            sha.update(data)
-    return sha.hexdigest()
-
-
-class Downloader():
-    """Downloader Class for EarthNet2021
-    """    
-
-    def __init__(self, data_dir: str, links:Dict[str,List[List[str]]]):
-        """Initialize Downloader Class
-
-        Args:
-            data_dir (str): The directory where the data shall be saved in, we recommend data/dataset/
-            links: dict of {split_type :[ [fname_1, link_1, sha_1], ... [fname_N, link_N, sha_N] ] }
-        """        
-        self.data_dir = data_dir
-        self.__URL__ = links
-        os.makedirs(data_dir, exist_ok = True)
-
-    @classmethod
-    def get(cls, data_dir: str, splits: Union[str,Sequence[str]], links:Dict[str,List[List[str]]], overwrite: bool = False, delete: bool = True):
-        """Download the EarthNet2021 Dataset
+def download(dataset = "earthnet2021x", split = "train", save_directory = "data/", proxy = None, limit = None):
+    """Download the recent EarthNet datasets
         
         Before downloading, ensure that you have enough free disk space. We recommend 1 TB.
 
-        Specify the directory data_dir, where it should be saved. Then choose, which of the splits you want to download.
-        All available splits: ["train","iid","ood","extreme","seasonal"]
-        You can either give "all" to splits or a List of splits, for example ["train","iid"].
+        Specify the directory `save_directory`, where it should be saved. Then choose, which of the splits you want to download.
+        All available splits: 
+            - For dataset `"earthnet2021x"`: `["train","iid","ood","extreme","seasonal"]`
+        
+        You can also give `"all"` to splits to download all splits of a particular dataset.
 
         Args:
-            data_dir (str): The directory where the data shall be saved in, we recommend data/dataset/
-            splits (Sequence[str]): Either "all" or a subset of ["train","iid","ood","extreme","seasonal"]. This determines the splits that are downloaded.
-            links: dict of {split_type :[ [fname_1, link_1, sha_1], ... [fname_N, link_N, sha_N] ] }
-            overwrite (bool, optional): If True, overwrites an existing gzipped tarball by downloading it again. Defaults to False.
-            delete (bool, optional): If True, deletes the downloaded tarball after unpacking it. Defaults to True.
-        """        
-        self = cls(data_dir, links)        
-        print(f"splits: {splits}")
+            dataset (str): The dataset you wish to download.
+            split (str): A split of the given dataset, can also be `"all"` to download all splits of a given dataset
+            save_directory (str): The directory where the data shall be saved in, we recommend data/
+            proxy (str, optional): If you need to use a http-proxy to access the internet, you may specify it here.
+            limit (int, optional): If you only want to download a certain number of samples, you can set a limit here.
+    """  
+    if split == "all":
+        for split in SPLITS[dataset]:
+            download(dataset = dataset, split = split, save_directory=save_directory, proxy = proxy, limit = limit)
+    else:
+        s3 = s3fs.S3FileSystem(anon=True,
+                client_kwargs={
+                'endpoint_url': 'https://s3.bgc-jena.mpg.de:9000',
+                'region_name': 'thuringia',
+                },
+                config_kwargs = {
+                "proxies": {'http': proxy}
+                } if proxy else {}
+            )
 
-        if isinstance(splits, str):
-            if splits == "all":
-                splits = ["train","iid","ood","extreme","seasonal"]
-        if isinstance(splits, str):
-            splits_set = {splits}
-            splits = (splits,)
-        elif isinstance(splits, list) and len(splits) == 1:
-            splits_set = {splits[0]}
-        else:
-            splits_set = set(splits)
-            
-        assert(splits_set.issubset(set(["train","iid","ood","extreme","seasonal"])))
+        print(f"Finding files of {dataset}, split {split} to download.")
+        files = s3.find(f"earthnet/{dataset}/{split}")
+        print(f"Downloading files of {dataset}, split {split}")
+        for file in tqdm(files[:limit] if limit else files):
+            savepath = Path(save_directory)/file[9:]
+            savepath.parent.mkdir(parents = True, exist_ok = True)
+            s3.download(file, str(savepath))
+        print(f"Downloaded {dataset}, split {split}.")
 
-        progress_file = os.path.join(self.data_dir, ".PROGRESS")
-        try:
-            with open(progress_file, "rb") as fp:
-                progress_list = pickle.load(fp)
-            print("Resuming Download.")
-        except:
-            progress_list = []
 
-        for split in splits:
-            print(f"Downloading split {split} to {self.data_dir}")
-            
-            for filename, dl_url, sha in tqdm(self.__URL__[split]):
-                if filename in progress_list and not overwrite:
-                    print(f"{filename} allready downloaded")
-                    continue
-                tmp_path = os.path.join(self.data_dir, filename)
-                print("Downloading...")
-                with DownloadProgressBar(unit='B', unit_scale=True, miniters=1, desc=filename) as t:
-                    urllib.request.urlretrieve(dl_url, filename = tmp_path, reporthook=t.update_to)
-                print("Downloaded!")
-                print("Asserting SHA256 Hash.")
-                assert(sha == get_sha_of_file(tmp_path))
-                print("SHA256 Hash is correct!")
-                print("Extracting tarball...")
-                with tarfile.open(tmp_path, 'r:gz') as tar:
-                    members = tar.getmembers()
-                    for member in tqdm(iterable=members, total=len(members)):
-                        tar.extract(member=member,path=self.data_dir)
-                print("Extracted!")
-                if delete:
-                    print("Deleting tarball...")
-                    os.remove(tmp_path)
-                
-                progress_list.append(filename)
-                with open(progress_file, "wb") as fp:
-                    pickle.dump(progress_list, fp)
+def load_minicube(dataset = "earthnet2021x", split = "train", id = "29SND_2018-09-03_2019-01-30_441_569_2745_2873_6_86_42_122", region = None, proxy = None):
+    """Load a minicube from a recent EarthNet dataset
+
+        Will give you a minicube loaded from the cloud.
+
+        All available splits: 
+            - For dataset `"earthnet2021x"`: `["train","iid","ood","extreme","seasonal"]`
+
+        Args:
+            dataset(str): The dataset
+            split (str): The split
+            id (str): The id of the minicube
+            region (str, optional): If you specify the region, downloading will be faster
+            proxy (str, optional): If you need to use a http-proxy to access the internet, you may specify it here.
+    
+    """
+    s3 = s3fs.S3FileSystem(anon=True,
+            client_kwargs={
+            'endpoint_url': 'https://s3.bgc-jena.mpg.de:9000',
+            'region_name': 'thuringia',
+            },
+            config_kwargs = {
+            "proxies": {'http': proxy}
+            } if proxy else {}
+        )
+    if region:
+        file = f"earthnet/{dataset}/{split}/{region}/{id}.nc"
+    else:
+        print(f"Searching for {id}...")
+        file = s3.glob(f"earthnet/{dataset}/{split}/**/{id}.nc")[0]
+        print(f"Found {id}.")
+    
+    mc = xr.open_dataset(s3.open(file))
+
+    return mc
+
+def load_en21x_as_npz(minicube_path):
+    """Load a minicube from the EarthNet2021x dataset as an EarthNet2021-like fake NPZ
+
+        Will return a dictionary with keys `['highresdynamic', 'highresstatic', 'mesodynamic', 'mesostatic']` as in the `.npz` files from EarthNet2021.
+        
+        Attention!
+            - `highresdynamic` has just 5 channels (like in the EarthNet2021 test sets)
+            - mesoscale data is just a single value repeated over the 80x80 mesoscale grid
+            - the COPDEM in EarthNet2021x has slightly higher resolution than the EU-DEM from EarthNet2021.
+            - This function returns a dictionary and not a `numpy.lib.npyio.NpzFile`
+
+        Args:
+            minicube_path (str): Path to the minicube from EarthNet2021x to be loaded as a fake NPZ.
+    
+    """
+
+    minicube = xr.open_dataset(minicube_path)
+
+    minicube["s2_mask"] = minicube.s2_mask.where(minicube.s2_mask == 0.0, 1.0)
+
+    hrd_fake = minicube[["s2_B02", "s2_B03", "s2_B04", "s2_B8A", "s2_mask"]].to_array("band").isel(time = slice(4, None, 5)).transpose("lat", "lon", "band", "time").values
+
+    eobs_shift = xr.DataArray(data = [0.0, -900., 50., 50., 50.], coords = {"var": ["eobs_rr", "eobs_pp", "eobs_tg", "eobs_tn", "eobs_tx"]})
+    eobs_scale = xr.DataArray(data = [50., 200., 100., 100., 100.], coords = {"var": ["eobs_rr", "eobs_pp", "eobs_tg", "eobs_tn", "eobs_tx"]})
+
+    md_fake = ((minicube[["eobs_rr", "eobs_pp", "eobs_tg", "eobs_tn", "eobs_tx"]].to_array("var") + eobs_shift)/eobs_scale).transpose("var", "time").values[None, None, :, :].repeat(80, 0).repeat(80, 1)
+
+    hrs_fake = (minicube.cop_dem.values[:, :, None] + 2000)/4000
+
+    ms_fake = np.nanmean(hrs_fake) * np.ones((80, 80, 1))
+
+    npz_fake = {
+        'highresdynamic': hrd_fake,
+        'highresstatic': hrs_fake,
+        'mesodynamic': md_fake,
+        'mesostatic': ms_fake
+    }
+
+    return npz_fake
 
 
 
 if __name__ == "__main__":
+    dataset = "earthnet2023"
+    save_directory = "/home/wph52/greenearthnet"
+    limit = 3
 
-    SPLITS = ["train"]
-    DATA_DIR = "/home/wph52/greenearthnet"
+    #download(dataset=dataset, save_directory=save_directory, limit=limit)
+    file = "/home/wph52/greenearthnet/earthnet2023/train/Algeria/29RNL6732.nc"
+    mc = xr.open_dataset(file)
+    breakpoint()
 
-    # only the first dataset links 
-    first_links = {key: [DOWNLOAD_LINKS[key][0]] for key in DOWNLOAD_LINKS.keys()}
 
-    Downloader.get(data_dir=DATA_DIR, splits=SPLITS, links=first_links)
+
+ 
---
+++ causal-earth/scripts/download/test.py
@@ -0,0 +1,12 @@
+import xarray as xr
+import os
+import numpy as np
+
+filepath = "/home/wph52/greenearthnet/train/34VFQ/34VFQ_2018-05-02_2018-09-28_4921_5049_5305_5433_76_156_82_162.npz"
+
+print(os.path.exists(filepath))
+#y = xr.open_dataset(filepath)
+x = np.load(filepath)
+print(x.files)
+
+breakpoint()
